e7992d145e1087dfd8c3c2001a575ca403962d42
cnachen <cnachencli@live.cn>
2025-02-07 19:37:07 +0800

    add: ckman 5.4.0

---
+++ Formula/ckman.rb
@@ -0,0 +1,80 @@
+class CanokeyManager < Formula
+  include Language::Python::Virtualenv
+
+  desc "Tool for managing your CanoKey configuration"
+  homepage "https://github.com/canokeys/yubikey-manager"
+  url "https://files.pythonhosted.org/packages/c2/e9/c5dbd9dc4def20e3a16974a115252a718fe8c4134e9cb50aaaa1762d828f/canokey_manager-5.4.0.tar.gz"
+  sha256 "2b459ade67eef968ce1a7a64a80788a0e293434041ef3f0874d5163a84df6786"
+  license "BSD-2-Clause"
+
+  depends_on "python3"
+
+  resource "cffi" do
+    url "https://files.pythonhosted.org/packages/fc/97/c783634659c2920c3fc70419e3af40972dbaf758daa229a7d6ea6135c90d/cffi-1.17.1.tar.gz"
+    sha256 "1c39c6016c32bc48dd54561950ebd6836e1670f2ae46128f67cf49e789c52824"
+  end
+
+  resource "click" do
+    url "https://files.pythonhosted.org/packages/b9/2e/0090cbf739cee7d23781ad4b89a9894a41538e4fcf4c31dcdd705b78eb8b/click-8.1.8.tar.gz"
+    sha256 "ed53c9d8990d83c2a27deae68e4ee337473f6330c040a31d4225c9574d16096a"
+  end
+
+  resource "cryptography" do
+    url "https://files.pythonhosted.org/packages/91/4c/45dfa6829acffa344e3967d6006ee4ae8be57af746ae2eba1c431949b32c/cryptography-44.0.0.tar.gz"
+    sha256 "cd4e834f340b4293430701e772ec543b0fbe6c2dea510a5286fe0acabe153a02"
+  end
+
+  resource "fido2" do
+    url "https://files.pythonhosted.org/packages/eb/cc/4529123364d41f342145f2fd775307eaed817cd22810895dea10e15a4d06/fido2-1.2.0.tar.gz"
+    sha256 "e39f95920122d64283fda5e5581d95a206e704fa42846bfa4662f86aa0d3333b"
+  end
+
+  resource "jaraco.classes" do
+    url "https://files.pythonhosted.org/packages/06/c0/ed4a27bc5571b99e3cff68f8a9fa5b56ff7df1c2251cc715a652ddd26402/jaraco.classes-3.4.0.tar.gz"
+    sha256 "47a024b51d0239c0dd8c8540c6c7f484be3b8fcf0b2d85c13825780d3b3f3acd"
+  end
+
+  resource "keyring" do
+    url "https://files.pythonhosted.org/packages/ae/6c/bd2cfc6c708ce7009bdb48c85bb8cad225f5638095ecc8f49f15e8e1f35e/keyring-24.3.1.tar.gz"
+    sha256 "c3327b6ffafc0e8befbdb597cacdb4928ffe5c1212f7645f186e6d9957a898db"
+  end
+
+  resource "more-itertools" do
+    url "https://files.pythonhosted.org/packages/88/3b/7fa1fe835e2e93fd6d7b52b2f95ae810cf5ba133e1845f726f5a992d62c2/more-itertools-10.6.0.tar.gz"
+    sha256 "2cd7fad1009c31cc9fb6a035108509e6547547a7a738374f10bd49a09eb3ee3b"
+  end
+
+  resource "pycparser" do
+    url "https://files.pythonhosted.org/packages/1d/b2/31537cf4b1ca988837256c910a668b553fceb8f069bedc4b1c826024b52c/pycparser-2.22.tar.gz"
+    sha256 "491c8be9c040f5390f5bf44a5b07752bd07f56edf992381b05c701439eec10f6"
+  end
+
+  resource "pyscard" do
+    url "https://files.pythonhosted.org/packages/fe/19/73e8fc37356a4232f9a86acf3794a0f6d8e014430f1f88153ae19fc21d88/pyscard-2.2.1.tar.gz"
+    sha256 "920e688a5108224cb19b915c3fd7ea7cf3d1aa379587ffd087973e84c13f8d94"
+  end
+
+  def install
+    # Fixes: smartcard/scard/helpers.c:28:22: fatal error: winscard.h: No such file or directory
+    ENV.append "CFLAGS", "-I#{Formula["pcsc-lite"].opt_include}/PCSC" if OS.linux?
+
+    venv = virtualenv_install_with_resources without: "pyscard"
+    # Use brewed swig
+    # https://github.com/Homebrew/homebrew-core/pull/176069#issuecomment-2200583084
+    # https://github.com/LudovicRousseau/pyscard/issues/169#issuecomment-2200632337
+    resource("pyscard").stage do
+      inreplace "pyproject.toml", 'requires = ["setuptools","swig"]', 'requires = ["setuptools"]'
+      venv.pip_install Pathname.pwd
+    end
+
+    # Still man/ykman.1 in upstream
+    # man1.install "man/ckman.1"
+
+    # Click doesn't support generating completions for Bash versions older than 4.4
+    generate_completions_from_executable(bin/"ckman", shells: [:fish, :zsh], shell_parameter_format: :click)
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/ckman --version")
+  end
+end
